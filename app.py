d2a0eaa1810d4eb3ba8761e672d84719fbdc8d40
Tetyana <iltaev2010@gmail.com>
2021-08-07 11:34:11 -0500

    test

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# just to check
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
