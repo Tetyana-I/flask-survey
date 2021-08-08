5381509a81f48b95be2bc55198e2bdd5dcb541c0
Tetyana <iltaev2010@gmail.com>
2021-08-07 22:13:39 -0500

    commented debugger tool

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from surveys import surveys, satisfaction_survey, personality_quiz
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret_code_here"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-debug = DebugToolbarExtension(app)
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def choose_the_servey():
@@ -17,33 +17,22 @@ def choose_the_servey():
 def start_survey():
     """render a page for the chosen survey that shows the user the title of the survey, the instructions, and a button to start the survey;
     setup session variables """
-    # current_survey=surveys[f"{request.form.get('choice')}"] #current survey object
     current_survey=surveys[request.form.get('choice')] #current survey object
-
     session['title'] = current_survey.title
     session['num_of_questions'] = len(current_survey.questions)
     session['survey'] = request.form['choice'] # key of current survey in surveys list
     session['comments'] = []
-    # session['responses'] = []
     return render_template('start.html', instructions = current_survey.instructions)
 
 @app.route('/start', methods=['POST'])
 def handling_start():
-    """ Set a current session to an empty list and redirect to the start of the survey """
+    """ Set a current session responses-list to an empty list and redirect to the start of the survey """
     session['responses'] = []
-    # answers = session.get('responses')
-    # if (not answers) or (len(answers) >= session['num_of_questions']):
-    #     session['responses'] = []
-    #     question = 0
-    # else:
-    #     question = len(responses)  
-    # url = f"/questions/{question}"  
-
     return redirect('/questions/0')
 
 @app.route('/questions/<question_number>')
 def question_page(question_number):
-    """ Page shows a form asking the current question, and listing the choices as radio buttons """
+    """ Page shows a form asking the current question, and listing the choices as radio buttons, also comments if are allowed """
     answers = session['responses']
     if len(answers) == session['num_of_questions']:
         return render_template('thanks.html')
@@ -52,7 +41,6 @@ def question_page(question_number):
     else:
         flash('Could you please answer this question, you tried to access an invalid question','error')
         number = len(answers)
- 
     current_survey = surveys[session['survey']] 
     current_question = current_survey.questions[number]
     return render_template('question.html', number = number, choices = current_question.choices, question = current_question.question, title = current_survey.title, allow_text = current_question.allow_text)
@@ -60,7 +48,7 @@ def question_page(question_number):
 
 @app.route('/answer', methods=['POST'])
 def handling_answer():
-    """ function appends the answer to responses list, and then redirect user to the next question."""
+    """ function appends the answer to responses list, adds comments if necessary and then redirect user to the next question; if no more questions in the survey - render thanks page ."""
     comment = request.form.get('comment')
     current_answer = request.form.get('choice')
     answers = session['responses']
